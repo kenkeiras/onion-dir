7f1ee29c166f23cc4d8eb6c5074c54ba477479e5
kenkeiras <kenkeiras@gmail.com>
2013-06-13 20:12:36 +0200

    Decoupled from command line interface

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Onion-Dir
 =========
 
-Resolve names through [Tor]("https://www.torproject.org/") so no one listening
+Resolve names through [Tor](https://www.torproject.org/) so no one listening
 the network nor even the ISP sniffs it.
+
+Resolution is done using the [Tor proxy interface](http://download.exdat.com/viewprogramfile/18551-22).
--- onion-dir.py
+++ onion-dir.py
@@ -1,20 +1,64 @@
 #!/usr/bin/env python
 #coding: utf-8
 
-import subprocess
+import struct
+from socket import socket
 
 from twisted.internet import defer, reactor
 from twisted.names import server, dns
 from twisted.names.common import ResolverBase
 
+# Tunable values
+PORT = 53
+TOR_SERVER = ("127.0.0.1", 9050)
+TIMEOUT = 3600
+
+# Protocol specific values
+IPv4_Addr = 1
+IPv6_Addr = 4
+Tor_Succeed = 0 # Query succeeded
+
+def unpack_ipv4_addr(raw_value):
+    '''Converts a raw binary IPv4 address into it's dot-separated form.'''
+    return '.'.join(map(lambda x: str(ord(x)), raw_value))
+
+def unpack_ipv6_addr(raw_value):
+    '''Converts a raw binary IPv6 address into it's colon-separated form.'''
+    return ':'.join(map(lambda x: hex(ord(x)), raw_value))
+
+def tor_lookup(sock, name):
+    '''Looks up a name through a Tor socks5 server.'''
+    name_len = struct.pack("B", len(name))
+
+    sock.sendall("\x05\x01\x00") # Authtentication (lack of)
+    authentication_check = sock.recv(2) # Should be 0x05 0x00 for SOCKS 5, no authentication
+
+    # 0xF0 is Tor proxy RESOLVE command
+    sock.sendall("\x05\xF0\x00\x03" + name_len + name + "\x00\x00")
+    answer_confirmation = sock.recv(4)
+    version, code, reserved, addr_type = struct.unpack("BBBB", answer_confirmation)
+
+    if code != Tor_Succeed:
+        return False
+
+    if addr_type == IPv4_Addr:
+        return unpack_ipv4_addr(sock.recv(4))
+
+    elif addr_type == IPv6_Addr:
+        return unpack_ipv6_addr(sock.recv(16))
+
 class TorResolver(ResolverBase):
     def _lookup(self, name, cls, type, timeout=10):
-        proc = subprocess.Popen(["tor-resolve", name],
-                                stdout=subprocess.PIPE)
+        sock = socket()
+        sock.connect(TOR_SERVER)
+        result = tor_lookup(sock, name)
+        sock.close()
 
-        response = proc.stdout.read().strip()
+        if not result:
+            return defer.succeed([(), (), ()])
+
         return defer.succeed([
-            (dns.RRHeader(name, dns.A, dns.IN, 3600, dns.Record_A(response, 3600)),), (), (),
+            (dns.RRHeader(name, dns.A, dns.IN, TIMEOUT, dns.Record_A(result, TIMEOUT)),), (), (),
             ])
 
 
@@ -22,5 +66,5 @@ resolver = TorResolver()
 factory = server.DNSServerFactory(clients=[resolver])
 protocol = dns.DNSDatagramProtocol(factory)
 
-reactor.listenUDP(53, protocol)
+reactor.listenUDP(PORT, protocol)
 reactor.run()
